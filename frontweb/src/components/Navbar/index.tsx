dd30904340419a937fdab6e4ff39988a954ae424
Lucas Eduardo <l.515@hotmail.com>
2022-10-06 17:55:15 -0300

    navbar start

---
+++ frontweb/src/components/Navbar/index.tsx
@@ -0,0 +1,27 @@
+import './styles.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-md bg-primary main-nav">
+      <div className="container-fluid">
+        <a href="link" className="nav-logo-text">
+          <h4>Carros Top</h4>
+        </a>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav offset-md-10 main-menu">
+            <li>
+              <a href="link" className="active">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="link">Catálogo</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
